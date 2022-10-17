b878befbd95a8b35d8f572c5cddcc1bcd1d7beaf
Henadz Buhlakou <g6834128@gmail.com>
2022-10-17 23:15:34 +0300

    Problem of the day 10_17_22_solved

---
+++ ProblemOfTheDay_101722.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+    public class ProblemOfTheDay_101722 {
+
+    /** Reversing input numbers
+     *
+     * Write a program that reads two integer numbers
+     * from the standard input and outputs them in the reverse order separated by one space.
+     */
+            public static void main(String args[]){
+                String n, n2;
+                Scanner in = new Scanner(System.in);
+                n = in.next();
+                n2 = in.next();
+                System.out.print(n2 + " " + n);
+            }
+    }
+
+
+
+
+
+
